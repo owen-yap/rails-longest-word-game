4d436359f47d1c3868d08d8bfe52fec41340cf92
Owen Yap <owen.y.yuhan@gmail.com>
2020-11-23 17:00:56 +0800

    Finished the challenge

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,3 +1,6 @@
+require 'open-uri'
+require 'json'
+
 class GamesController < ApplicationController
   def new
     alphabets = ('A'..'Z').to_a
@@ -8,6 +11,30 @@ class GamesController < ApplicationController
   end
 
   def score
-    raise
+    @word = params[:attempt].upcase
+    @response = 'win'
+    @letters = params[:letters].split('')
+    attempt = @word.split('')
+    attempt.each do |letter|
+      if @letters.find_index(letter)
+        @letters.delete_at(@letters.find_index(letter))
+      else
+        @response = 'invalid'
+      end
+    end
+    @response = 'not a word' if isnotvalid?
+  end
+
+  private
+
+  def isnotvalid?
+    url = "http://wagon-dictionary.herokuapp.com/#{@word}"
+    file = open(url).read
+    file_doc = JSON.parse(file)
+    if file_doc['found']
+      false
+    else
+      true
+    end
   end
 end
